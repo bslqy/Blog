3f937a0768bfd7e8092b22dd94478615e0089667
bslqy <853185345@qq.com>
2020-09-28 00:10:22 +1000

    Added user router

---
+++ blog-1/src/controller/user.js
@@ -0,0 +1,13 @@
+/*
+This layer is used to control the Data  transfer
+*/
+
+const loginCheck = (username,password) => {
+ // 先使用假数据
+ if (username === 'zhangsan' && password === '123'){
+     return true
+ }
+ return false
+}
+
+module.exports = {loginCheck}
